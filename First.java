d4bad497b8a6f31f74907a19e539e7e56c995905
shivteja1597 <tejashiv123@gmail.com>
2024-01-21 17:23:26 +0530

    Added all java files

---
+++ First.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class First {
+      public static void main(String[] args) {
+        //System.out.println("hi!!");
+    //Addition of two numbers and priting them
+   // int a=10,b=20,c;
+   // c=a+b;
+   // System.out.println(a);
+   // System.out.println(b);
+   // System.out.println("addition of a and b is " +c);
+   
+   //taking input from user
+  // Scanner f = new Scanner(System.in);
+  // System.out.print("Enter a num1 :- ");
+  // int a = f.nextInt();
+  // System.out.print("Enter num2 :- ");
+  // int b = f.nextInt();
+   //int c =a+b;
+   //System.out.println("Addition of a and b is " +c);
+
+Scanner s = new Scanner(System.in);
+
+System.out.print("Enter your number :- ");
+
+System.out.println(s.hasNextInt());
+
+
+
+}
+
+   
+    }
+    
+
